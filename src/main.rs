362d0e55c6b58eff6e3537ed3d1454a86784b6e1
Yash Agrawal <yagrawal2023@gmail.com>
2025-03-25 20:24:26 -0400

    refactored args

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
 fn main() {
     // Parse command line arguments
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {} in file {}", config.query, config.file_path);
 
     // Read file
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file at {file_path}");
 
     println!("File conents:\n{contents}");
 }
 
-fn parse_config(args: &Vec<String>) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+fn parse_config(args: &Vec<String>) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config {query, file_path}
 }
