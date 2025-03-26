e3cce31343f53f28845749c180d733a40ac5d1b1
Yash Agrawal <yagrawal2023@gmail.com>
2025-03-25 20:30:14 -0400

    config impl

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 use std::env;
 use std::fs;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
 fn main() {
     // Parse command line arguments
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {} in file {}", config.query, config.file_path);
 
@@ -20,8 +15,15 @@ fn main() {
     println!("File conents:\n{contents}");
 }
 
-fn parse_config(args: &Vec<String>) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config {query, file_path}
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Self {query, file_path}
+    }
 }
