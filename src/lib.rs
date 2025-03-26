39c45a7d243386597b3f19227a5e80ffa0557b2f
Yash Agrawal <yagrawal2023@gmail.com>
2025-03-25 21:30:48 -0400

    search

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use std::{fs, error::Error};
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() != 3 {
+            return Err("Incorrect number of arguments")
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Self {query, file_path})
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // Read file
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("File contents: \n{contents}");
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg (test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\nRust:\nsafe, fast, productive.\nPick three.";
+
+        let result = search(query, contents);
+        assert_eq!(vec!["safe, fast, productive."], result);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process, error::Error};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     // Parse command line arguments
@@ -11,33 +13,8 @@ fn main() {
 
     println!("Searching for {} in file {}", config.query, config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // Read file
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("File conents:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() != 3 {
-            return Err("Incorrect number of arguments")
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Self {query, file_path})
     }
 }
