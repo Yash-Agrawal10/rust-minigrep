f659f158872db54f7a18e1921bda20f952d91a3a
Yash Agrawal <yagrawal2023@gmail.com>
2025-03-25 20:45:44 -0400

    refactored config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::env;
-use std::fs;
+use std::{env, fs, process};
 
 fn main() {
     // Parse command line arguments
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        println!("Usage: ./minigrep <query> <filepath>");
+        process::exit(1);
+    });
 
     println!("Searching for {} in file {}", config.query, config.file_path);
 
     // Read file
     let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file at {file_path}");
+        .expect("Should have been able to read the file");
 
     println!("File conents:\n{contents}");
 }
@@ -21,9 +24,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() != 3 {
+            return Err("Incorrect number of arguments")
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Self {query, file_path}
+        Ok(Self {query, file_path})
     }
 }
